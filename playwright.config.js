0c637b4656ea6c2f18f9a3cdfc91df7a6ee2a005
Jesi Freire <jesianef@hotmail.com>
2025-06-07 11:01:11 -0300

    Adjust format code

--- playwright.config.js
+++ playwright.config.js
@@ -22,14 +22,17 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
-  reporter: [['html', { open: false }], ['junit', { outputFile: `./test-results/results.xml` }]],
+  reporter: [
+    ['html', { open: false }],
+    ['junit', { outputFile: `./test-results/results.xml` }],
+  ],
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: 'https://pgats-ci-example.netlify.app',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
-    screenshot: 'only-on-failure'
+    screenshot: 'only-on-failure',
   },
 
   /* Configure projects for major browsers */
